e585e8b2cdf7dcb6b2d2dd77420149ffbd5828b8
heej <sisesu92@gmail.com>
2020-05-14 23:55:44 +0900

    게시글 상세보기 추가

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board
+from .forms import BoardForm
+from fcuser.models import Fcuser
 # Create your views here.
+
+def board_detail(request, pk): # 게시글 아이디 사용
+    board = Board.objects.get(pk=pk)
+    return render(request, 'board_detail.html', {'board':board})
+
+def board_write(request):
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            user_id = request.session.get('user')
+            fcuser = Fcuser.objects.get(pk=user_id)
+
+            board = Board()
+            board.title = form.cleaned_data['title']
+            board.contents = form.cleaned_data['contents']
+            board.writer = fcuser
+            board.save()
+
+            return redirect('/board/list/')
+    else:
+        form = BoardForm()
+
+    return render(request, 'board_write.html', {'form':form})
 
 def board_list(request):
 
