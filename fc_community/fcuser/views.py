40fb747576a8ed07e7ee9f36e8f6a9a1d8cbd246
heej <sisesu92@gmail.com>
2020-05-14 20:46:40 +0900

    로그인 검증

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.hashers import check_password, make_password
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-
+from .forms import LoginForm
 from .models import Fcuser
 
 # Create your views here.
@@ -22,26 +22,16 @@ def logout(request):
     return redirect('/')
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
 
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야 합니다.'
-        else:
-            fcuser = Fcuser.objects.get(username=username)
-            if check_password(password, fcuser.password):
-                # 로그인 처리
-                request.session['user'] = fcuser.id
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호가 일치하지 않습니다.'
-
-        return render(request, 'login.html', res_data)
+        if form.is_valid(): # 검증
+            # session
+            return redirect('/')
+    else:
+        form = LoginForm()
+        
+    return render(request, 'login.html', {'form':form})
 
 
 def register(request):
